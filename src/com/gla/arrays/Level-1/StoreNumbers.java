d111cdc543b96170afaee2183f1fcd6306abfe3f
Ishant Pathak <ishantpathak25@gmail.com>
2026-02-04 22:14:33 +0530

    [Ishant]Add .StoreNumbers program
    This program allows users to input up to 10 positive numbers, displays the entered numbers, and calculates their sum.

---
+++ src/com/gla/arrays/Level-1/StoreNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class StoreNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] arr = new double[10]; 
+        double sum = 0;
+        int i = 0;
+        while (i < 10) {
+            System.out.print("Enter a number (0 or negative to stop): ");
+            double num = sc.nextDouble();
+            if (num <= 0) {
+                break; 
+            }
+            arr[i] = num;
+            i++;
+        }
+        System.out.println("\nNumbers entered:");
+        for (int j = 0; j < i; j++) {
+            System.out.println(arr[j]);
+            sum += arr[j];
+        }
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
