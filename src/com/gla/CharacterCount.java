16ca292505c7bafd3bfe9d5026af3b43e9d2c8af
Ishant Pathak <ishantpathak25@gmail.com>
2026-04-03 23:34:00 +0530

    [Ishant] Add. CharacterCount program to count string elements
    This program counts the total number of letters, digits, and special characters in a user-input string.

---
+++ src/com/gla/CharacterCount.java
@@ -0,0 +1,32 @@
+package com.gla.WrapperClass;
+
+import java.util.Scanner;
+
+public class CharacterCount {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int letters = 0, digits = 0, special = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (Character.isLetter(ch)) {
+                letters++;
+            } else if (Character.isDigit(ch)) {
+                digits++;
+            } else if (!Character.isWhitespace(ch)) {
+                special++;
+            }
+        }
+
+        System.out.println("Total letters = " + letters);
+        System.out.println("Total digits = " + digits);
+        System.out.println("Total special characters = " + special);
+
+        sc.close();
+    }
+}
